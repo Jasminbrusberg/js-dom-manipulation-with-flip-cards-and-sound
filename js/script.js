59648b58e2f326e58467ea6631085009786ad62c
Jasminbrusberg <J-brusberg@hotmail.com>
2026-02-26 12:48:50 +0100

    2-modul

--- js/script.js
+++ js/script.js
@@ -37,3 +37,38 @@ if (sectionOne) {
 
 // NYT NYT Vanilla JS
 
+// Laver nogle variabler som refererer til de pågældende lyd-filer
+const soundDog = new Audio("../sound/dog.wav");
+const soundHorse = new Audio("../sound/horse.wav");
+const soundBear = new Audio("../sound/bear.wav");
+
+const dogCard = document.getElementById("dogCard")
+const horseCard = document.getElementById("horseCard")
+const bearCard = document.getElementById("bearCard")
+
+if (dogCard) 
+{
+  dogCard.addEventListener ("click", function(){
+    soundDog.play();
+    dogCard.classList.toggle ("flipped");
+  });
+}
+
+if (horseCard) 
+{
+  horseCard.addEventListener ("click", function(){
+    soundHorse.play();
+    horseCard.classList.toggle ("flipped");
+  });
+}
+
+if (bearCard) 
+{
+  bearCard.addEventListener ("click", function(){
+    soundBear.play();
+    bearCard.classList.toggle ("flipped");
+  });
+}
+
+
+
